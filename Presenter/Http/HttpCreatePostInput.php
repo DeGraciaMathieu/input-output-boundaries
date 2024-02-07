7494bfca7ec24c52a1262ebacfb329b5ab409a3f
DeGraciaMathieu <degracia.mathieu@gmail.com>
2024-02-07 08:23:20 +0100

    Update HttpCreatePostInput.php

--- Presenter/Http/HttpCreatePostInput.php
+++ Presenter/Http/HttpCreatePostInput.php
@@ -4,6 +4,10 @@ namespace Presenter\Http;
 
 use Application\UsesCases\CreatePostInput;
 
+/**
+* It represents an implementation of the necessary inputs for the action to work. 
+* This class allows for the inversion of dependence between the presenter layer and the application layer.
+*/
 class HttpCreatePostInput implements CreatePostInput
 {
     public function __construct(
